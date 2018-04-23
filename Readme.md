a8c0b0a0b96b04403bd99129efb79ff1708bc1ef
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# Report Designer for WPF - How to switch the Report Wizard to a single-query version
+
+
+Starting with version 17.1, we have introduced a new Report Wizard version that enables you to create hierarchical master-detail data sources with a single wizard run. <br><br>This example demonstrates how to switch the Report Wizard to an older version, which allows your end-users to select data only from a single query at once. <br><br>
+<p>The following images illustrate some pages of a single-query Report Wizard.</p>
+<br><img src="https://raw.githubusercontent.com/DevExpress-Examples/report-designer-for-wpf-how-to-switch-the-report-wizard-to-a-single-query-version-t506224/17.1.3+/media/75b0ad3f-2a6f-11e7-80c0-00155d624807.png"><br><br><img src="https://raw.githubusercontent.com/DevExpress-Examples/report-designer-for-wpf-how-to-switch-the-report-wizard-to-a-single-query-version-t506224/17.1.3+/media/797b151f-2a6f-11e7-80c0-00155d624807.png"><br><br><img src="https://raw.githubusercontent.com/DevExpress-Examples/report-designer-for-wpf-how-to-switch-the-report-wizard-to-a-single-query-version-t506224/17.1.3+/media/7db37070-2a6f-11e7-80c0-00155d624807.png">
+
+<br/>
+
+
---
+++ VB/SingleQueryWizard/MyWizardCustomizationService.vb
@@ -0,0 +1,36 @@
+﻿Imports DevExpress.Xpf.Reports.UserDesigner.ReportWizard
+Imports DevExpress.DataAccess.Wizard.Model
+Imports DevExpress.Xpf.DataAccess.DataSourceWizard
+Imports DevExpress.XtraReports.UI
+Imports DevExpress.XtraReports.Wizards
+Imports DevExpress.DataAccess.Wizard
+Imports System.Drawing
+
+Namespace SingleQueryWizard
+    Public Class MyWizardCustomizationService
+        Implements IWizardCustomizationService
+
+        Private Sub IDataSourceWizardCustomizationService_CustomizeDataSourceWizard(ByVal customization As DataSourceWizardCustomizationModel, ByVal container As ViewModelSourceIntegrityContainer) Implements IDataSourceWizardCustomizationService.CustomizeDataSourceWizard
+            Dim wizardOptions = container.Resolve(Of SqlWizardOptions)()
+            wizardOptions = wizardOptions And Not SqlWizardOptions.MultiQueryWizard 'or you can use 'var wizardOptions = SqlWizardOptions.None'
+            container.RegisterInstance(wizardOptions)
+        End Sub
+
+        Private Sub IWizardCustomizationService_CustomizeReportWizard(ByVal customization As ReportWizardCustomizationModel, ByVal container As ViewModelSourceIntegrityContainer) Implements IWizardCustomizationService.CustomizeReportWizard
+            Dim wizardOptions = container.Resolve(Of SqlWizardOptions)()
+            wizardOptions = wizardOptions And Not SqlWizardOptions.MultiQueryWizard 'or you can use 'var wizardOptions = SqlWizardOptions.None'
+            container.RegisterInstance(wizardOptions)
+        End Sub
+
+        Private Function IDataSourceWizardCustomizationService_TryCreateDataSource(ByVal model As IDataSourceModel, ByRef dataSource As Object, ByRef dataMember As String) As Boolean Implements IDataSourceWizardCustomizationService.TryCreateDataSource
+            dataSource = Nothing
+            dataMember = Nothing
+            Return False
+        End Function
+
+        Private Function IWizardCustomizationService_TryCreateReport(ByVal model As XtraReportModel, ByRef report As XtraReport) As Boolean Implements IWizardCustomizationService.TryCreateReport
+            report = Nothing
+            Return False
+        End Function
+    End Class
+End Namespace
